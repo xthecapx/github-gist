e7889ebb326d39d03d7050dfd4b8c248f5747d7f
xthecapx <xthecapx@gmail.com>
2021-03-13 10:05:19 -0500

    feat: css modules and redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { store } from './store';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
